643328dbebb3eebc9b93a632772304525491e893
SafwatRahman <safwatafif@gmail.com>
2020-03-07 23:17:03 -0600

    first commit

---
+++ src/components/googleCalender.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class GoogleCalender extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <button>Google Calender</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default GoogleCalender;
---
+++ src/components/intro.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Intro extends Component {
+  state = {};
+  render() {
+    return (
+      <h1>
+        Hi, there. I'm Eoma and I'm here to help you decide what to eat. My
+        philosophy of choosing what to eat depends on what I will be doing after
+        I eat. For example, if I have a session of productivity planned right
+        after eating, I will suggest that you eat something light and healthy so
+        you don't feel tired and lazy from a big, fatty meal. <br></br>So, let
+        me know what you got planned for today!
+      </h1>
+    );
+  }
+}
+
+export default Intro;
